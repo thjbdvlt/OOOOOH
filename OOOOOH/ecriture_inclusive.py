45f23b3b6f0cc2c93f9da909d6740d2d8e388397
thjbbvlt <thibault2@ik.me>
2024-05-29 17:23:50 +0200

    écrit fonctions écriture inclusive

---
+++ OOOOOH/ecriture_inclusive.py
@@ -0,0 +1,106 @@
+def match_suff_nonbinary(s):
+    """Match (début de str) un suffixe non-binaire 'x'.
+
+    Args:
+        s (str): le suffixe dans lequel match 'x'.
+
+    Returns:
+        int: la longueur du match (donc 1) si un match est trouvé, sinon -1.
+    """
+
+    return 1 if s[0] == "x" else -1
+
+
+def match_suff_plural(s):
+    """Match (début de str) un suffixe pluriel 's'.
+
+    Args:
+        s (str): le suffixe dans lequel match 's'.
+
+    Returns:
+        int: la longueur du match (donc 1) si un match est trouvé, sinon -1.
+    """
+
+    return 1 if s[0] == "s" else -1
+
+
+def match_suff_feminine(s):
+    """Match en début de str un suffixe féminin.
+
+    Args:
+        s (str): le suffixe dans lequel un suffixe.
+
+    Returns:
+        int: la longueur du match si un match est trouvé, sinon -1.
+
+    Note:
+        l'ordre dans lequel sont placés, ci-dessous, les différents suffixes est important (c'est un ordre de priorité, il faut par exemple absolument éviter de mettre en première position 'e', qui empêcherait de match 'esse' et 'euse'.)
+    """
+
+    for i in (
+        "trice",
+        "rice",
+        "ice",
+        "euse",
+        "ère",
+        "oresse",
+        "esse",
+        "lle",
+        "le",
+        "tte",
+        "te",
+        "ive",
+        "ale",
+        "se",
+        "ne",
+        "e",
+    ):
+        if s.startswith(i):
+            return len(i)
+    return -1
+
+
+def split_suff_incl(suffix):
+    """Décompose un suffixe inclusif aggrégé.
+
+    Args:
+        suffix (str): le suffixe à décomposer.
+
+    Returns:
+        Union[list, str]
+
+    Exemples:
+        - "eusexs" -> ["euse", "x", "s"]
+        - "xeuse" -> ["x", "euse", "s"]
+
+    Si le suffixe ne peut pas être complétement décomposé, le suffixe est retourné tel quel, il ne faut donc pas passer des mots des mots entiers.
+        - "acteurice" -> "acteurice"
+        - "riceyyy" -> "rice"
+
+    La fonction définit un ordre dans lequel les suffixes peuvent apparaître (le pluriel ne peut pas se trouver avant le féminin), et chaque type de suffixe (féminin, non-binaire, pluriel) ne peut apparaître qu'une seule fois:
+        - "euseeuse" -> "euseeuse"
+        - "srice" -> "srice"
+    """
+
+    a = []
+    s = suffix
+    already = set()
+    functions = (
+        match_suff_nonbinary,
+        match_suff_feminine,
+        match_suff_nonbinary,
+        match_suff_plural,
+        match_suff_nonbinary,
+    )
+    for fn in functions:
+        if fn in already:
+            continue
+        suff_len = fn(s)
+        if suff_len != -1:
+            suff = s[:suff_len]
+            a.append(suff)
+            s = s[suff_len:]
+            if s == "":
+                return a
+            already.add(fn)
+    return suffix
