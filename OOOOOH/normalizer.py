6bcef134f049345576926f6caad203e7cc7870c9
thjbbvlt <thibault2@ik.me>
2024-05-30 12:01:55 +0200

    restructurer fonction normaliser_mot()

--- OOOOOH/normalizer.py
+++ OOOOOH/normalizer.py
@@ -1,5 +1,5 @@
 import re
-from spacy.lookups import Lookups
+from spacy.lookups import Table
 import OOOOOH.ecriture_inclusive
 import OOOOOH.default
 import OOOOOH.chars
@@ -21,14 +21,13 @@ class Normalizer:
             agg_suff (str, Callable):  Accepte soit une fonction définissant comment agréger les suffixes d'écriture inclusive, soit une chaîne de caractère permettant de définir comment utiliser les méthodes prédéfinies.
             use_default_word_list (bool):  utiliser (en plus des éventuels fichiers de mots en paramètres) la liste de mots integrée au package.
 
-        Returns:
-            None
+        Returns: None
 
         Exemple d'utilisation du paramètre `agg_suff`:
             '·': auteur·ricexs (défaut)
             '··': auteur·rice·x·s
-            '--': auteur-rice-x-s
             '..': auteur.rice.x.s
+            '-': auteur-ricexs (pas recommandé: moins performant)
             '_': auteur_ricexs
             None: autrices
         """
@@ -47,8 +46,7 @@ class Normalizer:
 
         # crée deux tables de lookup: une première qui contient tous les mots (et dans laquelle seront ajoutés les variantes orthographiques, à mesure de l'analyse), et une autre qui contient les versions désaccentués, pointant vers les normes (par exemple: {"etre": "être"}).
         d = {}
-        d_desacc = {}
-
+        d_noacc = {}
         lines = []
         if use_default_word_list is True:
             import pkgutil
@@ -59,7 +57,7 @@ class Normalizer:
                 .split("\n")
             )
 
-        # la valeur du paramètre 'words_files' est utilisée pour ajouter des mots à la liste.
+        # la valeur du paramètre 'words_files' est utilisée pour ajouter des mots à la liste. il peut s'agir soit d'un fichier unique (str) soit d'une liste de fichiers.
         if isinstance(words_files, (list, tuple, set)):
             for fp in words_files:
                 with open(fp, "r") as f:
@@ -72,12 +70,13 @@ class Normalizer:
         for l in lines:
             l = l.strip()
             d[l] = l
-            d_desacc[self.desaccentuer(l)] = l
+            d_noacc[self.desaccentuer(l)] = l
 
-        # utilise l'objet `Lookups` de spaCy, optimisé pour ces tâches.
-        self.lookup = Lookups()
-        self.lookup.add_table("normes", d)
-        self.lookup.add_table("normes_sans_accents", d_desacc)
+        # utilise l'objet `Table` de spaCy, optimisé pour le lookup.
+        self.index = Table(name="normes", data=d)
+        self.index_noacc = Table(
+            name="normes_sans_accents", data=d_noacc
+        )
 
         # compile le regex qui trouve des séquences de lettres identiques (3x ou plus la même lettre d'affilé)
         self.re_multi = re.compile(
@@ -119,20 +118,32 @@ class Normalizer:
         word = "".join(chars)
         return word
 
-    def reduire_multiple(self, word) -> str:
-        """Supprime les caractères répétées plus de trois fois.
+    def cherche_avec_ou_sans_accents(self, word, norm) -> str:
+        """cherche un mot dans les indexes (version accentuée et désaccentuée).
 
-        Args:
-            word (str): le mot.
+        (met à jour les indexes en même temps.)
 
-        Returns:
-            str: le mot, sans les lettres répétées.
+        args:
+            - word (str): le mot.
 
-        Exemple:
-            'haaaa' -> 'ha'
+        returns (str): la norme trouvée (le mot tel quel, si rien n'a été trouvé).
         """
 
-        return self.re_multi.sub(r"\1", word)
+        index = self.index
+        index_noacc = self.index_noacc
+        if word in index:
+            norm = index[word]
+            return norm
+
+        sub_noacc = self.desaccentuer(word)
+        if sub_noacc in index_noacc:
+            norm = index_noacc[sub_noacc]
+            index[word] = norm
+            return norm
+
+        index_noacc[word] = word
+        index[word] = word
+        return word
 
     def normaliser_mot(self, word) -> str:
         """Cherche ou construit la forme normalisée d'un mot.
@@ -144,61 +155,47 @@ class Normalizer:
             str: La forme normalisée du mot.
         """
 
-        index = self.lookup.get_table("normes")
-        index_desaccentue = self.lookup.get_table(
-            "normes_sans_accents"
-        )
+        index = self.index
 
-        # cherche si le mot est dans la table lookup.
+        # cherche si le mot est, tel quel, dans la table lookup.
         if word in index:
             return index[word]
 
-        # une copie du mot, qui va être modifiée successivement jusqu'à trouver une norme.
-        s = word
+        # les normes sont toujours en minuscules
+        word = word.lower()
 
         # reconstruit le mot, caractère par caractère. si un caractère est dans le set 'chars_todel', il est ignoré, s'il est dans le dict 'chars_toreplace' il est remplacé (ex. '·' par '-'). dans les autres cas, il est placé tel quel.
         rep = self.chars_toreplace
-        s = s.lower()
-        s = "".join(
+        word = "".join(
             [
                 rep[c] if c in rep else c
-                for c in s
+                for c in word
                 if c not in self.chars_todel
             ]
         )
 
         # enlève les répétitions de caractères (à partir de trois)
-        s = self.reduire_multiple(s)
+        word = self.re_multi.sub(r"\1", word)
+
+        # cherche si le mot est, après ces transformations de normalisations, dans l'index.
+        if word in index:
+            return index[word]
+
+        # une copie du mot pour des modifications destinées uniquement à chercher une norme (et non pas, comme dans les opérations précédentes, à la construire).
+        s = word
+
         if "-" in s:
             # word = self.enlever_suffixes(word)  # todo: implémenter
+            # si la string contient encore des tirets, il s'agit alors d'un mot composé
             if "-" in s:
                 x = s.split("-")
-                a = []
-                for i in x:
-                    if i in index:
-                        norm = index[i]
-                        a.append(norm)
-                        continue
-                    di = self.desaccentuer(i)
-                    if di in index_desaccentue:
-                        norm = index_desaccentue[di]
-                        a.append(norm)
-                        index[i] = norm
-                        continue
-                    a.append(i)
-                    index[i] = i
+                # enlève les élements vides, produits par exemples dans "-je" ou "peut--être".
+                x = [i for i in x if i != ""]
+                fn = self.cherche_avec_ou_sans_accents
+                a = [fn(sub) for sub in x]
                 s = "-".join(a)
 
-        if s in index:
-            return index[s]
-
-        y = index_desaccentue.get(self.desaccentuer(s))
-        if y is not None:
-            index.set(s, y)
-            return y
-
-        # index[word] = word
-        index.set(s, s)
+        s = fn(s)
         return s
 
     def __call__(self, doc):
