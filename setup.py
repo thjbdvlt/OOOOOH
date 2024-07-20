c2e9e3ddd13421a9e9fc9ef818ef9fe3c6c2b4d3
thjbbvlt <thibault2@ik.me>
2024-07-20 20:38:18 +0200

    setup.py: pas besoin import presque

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name="presque",
+    entry_points={
+        "spacy_factories": ["presque_normalizer = presque.normalizer:create_presque_normalizer"]
+    }
+)
